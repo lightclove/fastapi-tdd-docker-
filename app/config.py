a87f0ef6d514ca22edb22488f43931db8b1c2e00
Dmitriy ILiyushko <79217526217@ya.ru>
2024-04-26 06:39:47 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 from functools import lru_cache
 
 from pydantic_settings import BaseSettings
-from pydantic import AnyHttpUrl
+from pydantic import AnyUrl
 
 log = logging.getLogger("uvicorn")
 
@@ -11,7 +11,7 @@ log = logging.getLogger("uvicorn")
 class Settings(BaseSettings):
     environment: str = os.getenv("ENVIRONMENT", "dev")
     testing: bool = os.getenv("TESTING", 0)
-    database_url: AnyHttpUrl = os.environ.get("DATABASE_URL")
+    database_url: AnyUrl = os.environ.get("DATABASE_URL")
 
 
 @lru_cache()
